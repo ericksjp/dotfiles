90b1f27624c19adeb675b1e290e7e071f476d689
Erick <ericksjp703@gmail.com>
2024-03-24 14:27:50 -0300

    setting lsp config file

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim", --gerenciador de lsps
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "williamboman/mason-lspconfig.nvim", --plugin para configurar os lsps localmente
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" },
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig", --plugin para comunicação entre o nvim e os lsps
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
+      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+    end
+  }
+}
