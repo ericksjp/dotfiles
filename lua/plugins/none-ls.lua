4ae0944a8b73df16f98967171a86283dc4e47a05
Erick <ericksjp703@gmail.com>
2024-03-24 17:05:50 -0300

    fixing eslint_d in none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,19 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
+  "nvimtools/none-ls.nvim",
+  dependencies = {
+    "nvimtools/none-ls-extras.nvim",
+  },
 
-		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-	end,
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup{
+      sources = {
+        require("none-ls.diagnostics.eslint_d"),
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.stylua,
+      },
+    }
+
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
 }
